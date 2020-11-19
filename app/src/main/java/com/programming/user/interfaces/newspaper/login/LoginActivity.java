fa2c06ab2a721cebaedcf245ae871a9d3879d2b3
Patricia Vera H <p.vhernandez@hotmail.com>
2020-11-19 18:37:37 +0100

    Started login xml

--- app/src/main/java/com/programming/user/interfaces/newspaper/login/LoginActivity.java
+++ app/src/main/java/com/programming/user/interfaces/newspaper/login/LoginActivity.java
@@ -1,9 +1,17 @@
 package com.programming.user.interfaces.newspaper.login;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.programming.user.interfaces.newspaper.R;
 
 public class LoginActivity extends AppCompatActivity {
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 
 }
