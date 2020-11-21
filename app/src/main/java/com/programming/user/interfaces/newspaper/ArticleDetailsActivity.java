3d8728af28e5adc30e181546406784dd7fb2242f
Patricia Vera H <p.vhernandez@hotmail.com>
2020-11-21 01:56:14 +0100

    Files created

---
+++ app/src/main/java/com/programming/user/interfaces/newspaper/ArticleDetailsActivity.java
@@ -0,0 +1,20 @@
+package com.programming.user.interfaces.newspaper;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ArticleDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initialize();
+    }
+
+    private void initialize() {
+        
+    }
+}
